33b124b5eb9f2af70f64800cba42e25750140123
Federico <fedev93@gmail.com>
2018-10-02 18:54:16 +0200

    6 commit

--- public/assets/js/EM.js
+++ public/assets/js/EM.js
@@ -1,15 +1,60 @@
 
 $(document).ready(function () {
+    jQuery.validator.setDefaults({
+        debug: true,
+        success: "valid"
+    });
 
-    $("#loginbtn").on('click', function (e) {
-        e.preventDefault();
-        console.log("clicked! ", validateMail($("#input-mail").val()));
-    })
+    $("#loginform").validate({
+        rules: {
+            inputmail: {
+                required: true,
+                email: true,
+                minlength: 6
+            },
+            inputpass: {
+                required: true,
+                minlength: 3
+            }
+        },
+        messages: {
+            inputmail: {
+                required: "Questo campo è obbligatorio!",
+                email: "Inserisci un email valida!",
+                minlength: "La lunghezza minima è 6 caratteri!"
+
+            },
+            inputpass: {
+                required: "Questo campo è obbligatorio!",
+                minlength: "La lunghezza minima è 3 caratteri!"
+            }
+        },
+        submitHandler: function(form) {
+            var dati = {
+                email: $("#inputmail").val(),
+                password: $("#inputpass").val()
+            };
+            $.ajax({
+                url: "/login",
+                type: "POST",
+                data: JSON.stringify(dati),
+                cache: false,
+                processData: false,
+            });
+            return false;
+        }
+    });
+    //
+    // $("#loginbtn").on('click', function (e) {
+    //     e.preventDefault();
+    //     var user = {
+    //         email: $("#inputmail").val(),
+    //         password: $("#inputpass").val()
+    //     };
+    //     $.post('/login', user, function (res) {
+    //         console.log("post done");
+    //     });
+    // })
 
 });
-
-
-function validateMail(value) {
-        return (/[a-z]+@[a-z]+\.[a-z]+/.test(value));
-}
 
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var bodyparser = require('body-parser');
 var app = express();
 
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.json());
 app.use(express.static(__dirname + '/'));
 
 app.get('/login', (req, res) => {
     res.redirect('/public/login.html');
+});
+
+app.post('/login', function (req, res) {
+   if(data.length){
+       console.log(req.body.dati);
+   }
 });
 
 app.listen(8080);
