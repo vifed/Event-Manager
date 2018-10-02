6e4eb6128082afd087a055ded3ca788c8d99634b
Federico <fedev93@gmail.com>
2018-10-02 14:42:03 +0200

    5 commit

--- public/assets/js/EM.js
+++ public/assets/js/EM.js
@@ -1,7 +1,15 @@
 
 $(document).ready(function () {
 
-    $("#loginbtn").on('click', function () {
-        alert("clicked!");
+    $("#loginbtn").on('click', function (e) {
+        e.preventDefault();
+        console.log("clicked! ", validateMail($("#input-mail").val()));
     })
+
 });
+
+
+function validateMail(value) {
+        return (/[a-z]+@[a-z]+\.[a-z]+/.test(value));
+}
+
